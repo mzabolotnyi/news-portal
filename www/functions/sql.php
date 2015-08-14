9df1b10247feabb92ac384e0cd177a52ba2163e8
max <mzabolotnyi@gmail.com>
2015-08-14 16:25:20 +0300

    sql functions add

--- www/functions/sql.php
+++ www/functions/sql.php
@@ -1,2 +1,28 @@
 <?php
 
+function sql_connect()
+{
+    mysql_connect('localhost', 'root', '');
+    mysql_select_db('news');
+}
+
+function sql_select($query)
+{
+    sql_connect();
+    $res = mysql_query($query);
+    $ret = [];
+
+    while ($row = mysql_fetch_row($res)) {
+        $ret[] = $row;
+    }
+
+    return $ret;
+}
+
+function sql_execute($query)
+{
+    sql_connect();
+    $res = mysql_query($query);
+
+    return $res != false;
+}
